c44c2afb92d9f0f02b6b7e060d5b72287d615ad8
suyashhiray <46005766+suyashhiray@users.noreply.github.com>
2019-11-18 18:12:57 -0700

    Add files via upload

--- enroll.py
+++ enroll.py
@@ -13,32 +13,32 @@ def main():
   		img_name = input()
 	elif decision == 2: 
 		print("Camera is opening, press spacebar to take image\n")
-	  	cam = cv2.VideoCapture(0)
-	  	cv2.namedWindow("Enrollment")
+		cam = cv2.VideoCapture(0)
+		cv2.namedWindow("Enrollment")
 		while True:
-		    ret, frame = cam.read()
-		    cv2.imshow("Enrollment", frame)
-		    if not ret:
-		        break
-		    k = cv2.waitKey(1)
+			ret, frame = cam.read()
+			cv2.imshow("Enrollment", frame)
+			if not ret:
+			break
+			k = cv2.waitKey(1)
 
-		    if k%256 == 27:
+			if k%256 == 27:
 		        # ESC pressed
-		        print("Escape hit, closing...")
-		        break
-		    elif k%256 == 32:
+		    	print("Escape hit, closing...")
+		    	break
+			elif k%256 == 32:
 		        # SPACE pressed
-		        img_name = "{}.jpg".format(name)
-		        cv2.imwrite(img_name, frame)
-		        print("{} added to enrolled list!".format(img_name))
+				img_name = "{}.jpg".format(name)
+		    	cv2.imwrite(img_name, frame)
+				print("{} added to enrolled list!".format(img_name))
 
 			cam.release()
 
 			cv2.destroyAllWindows()
 
-  	f = open("enrolled.txt", "a")
-  	f.write(name + "^^" + img_name)
-  	f.close()
+	f = open("enrolled.txt", "a")
+	f.write(name + "^^" + img_name)
+	f.close()
 	for img_name in os.listdir(getcwd()):
     	if os.path.isfile(os.path.join(getcwd(), img_name)):
     		os.rename(os.path.join(getcwd(), img_name), os.path.join(os.getcwd() + "/Images", img_name))
