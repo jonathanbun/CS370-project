bbe2249786f1875944189d3312c1762384ff1287
suyashhiray <46005766+suyashhiray@users.noreply.github.com>
2019-11-18 18:41:08 -0700

    Update enroll.py

--- enroll.py
+++ enroll.py
@@ -2,15 +2,17 @@ import face_recognition
 import cv2
 import numpy as np
 import os
+import shutil
 
 def main():
+	img_name = ""
 	print("Welcome to enrollment! Please enter your name:\n")
 	name = input()
 	print("Enter '1' for uploading an image or '2' for taking an image with the webcam\n")
 	decision = input()
 	if decision == 1:
-  		print("Please enter image name with extension and place into folder:\n")
-  		img_name = input()
+		print("Please enter image name with extension and place into folder:\n")
+		img_name = input()
 	elif decision == 2: 
 		print("Camera is opening, press spacebar to take image\n")
 		cam = cv2.VideoCapture(0)
@@ -24,12 +26,12 @@ def main():
 
 			if k%256 == 27:
 		        # ESC pressed
-		    	print("Escape hit, closing...")
+				print("Escape hit, closing...")
 				break
 			elif k%256 == 32:
 		        # SPACE pressed
 				img_name = "{}.jpg".format(name)
-		    	cv2.imwrite(img_name, frame)
+				cv2.imwrite(img_name, frame)
 				print("{} added to enrolled list!".format(img_name))
 
 			cam.release()
@@ -37,12 +39,11 @@ def main():
 			cv2.destroyAllWindows()
 
 	f = open("enrolled.txt", "a")
-	f.write(name + "^^" + img_name)
+	f.write(name + "^^" + img_name + "\n")
 	f.close()
-	for img_name in os.listdir(getcwd()):
-    	if os.path.isfile(os.path.join(getcwd(), img_name)):
-    		os.rename(os.path.join(getcwd(), img_name), os.path.join(os.getcwd() + "/Images", img_name))
+	for file in os.listdir(os.getcwd()):
+		if file.endswith(".jpg"):
+			shutil.move(os.getcwd(), os.path.join(os.getcwd() + "/Images", file))
 
-  
 if __name__== "__main__":
 	main()
