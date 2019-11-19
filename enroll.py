3a2a4c999493359b850dbfa8665903a1593cf227
suyashhiray <46005766+suyashhiray@users.noreply.github.com>
2019-11-18 17:49:46 -0700

    Add files via upload

--- enroll.py
+++ enroll.py
@@ -3,19 +3,19 @@ import cv2
 import numpy as np
 
 def main():
-  print("Welcome to enrollment! Please enter your name:\n")
-  name = input()
-  print("Enter '1' for uploading an image or '2' for taking an image with the webcam\n")
-  decision = input()
-  if decision == 1:
-  	print("Please enter image name with extension and place into folder:\n")
-  	img_name = input()
-  elif decision == 2: 
-  	print("Camera is opening, press spacebar to take image\n")
+	print("Welcome to enrollment! Please enter your name:\n")
+	name = input()
+	print("Enter '1' for uploading an image or '2' for taking an image with the webcam\n")
+	decision = input()
+	if decision == 1:
+  		print("Please enter image name with extension and place into folder:\n")
+  		img_name = input()
+	elif decision == 2: 
+	  	print("Camera is opening, press spacebar to take image\n")
 
-	cam = cv2.VideoCapture(0)
+		cam = cv2.VideoCapture(0)
 
-	cv2.namedWindow("Enrollment")
+		cv2.namedWindow("Enrollment")
 
 	while True:
 	    ret, frame = cam.read()
@@ -34,17 +34,16 @@ def main():
 	        cv2.imwrite(img_name, frame)
 	        print("{} added to enrolled list!".format(img_name))
 
-	cam.release()
+		cam.release()
 
-	cv2.destroyAllWindows()
+		cv2.destroyAllWindows()
 
-  f = open("enrolled.txt", "a")
-  f.write(name + "^^" + img_name)
-  f.close()
-
-  for img_name in os.listdir(getcwd()):
-      if os.path.isfile(os.path.join(getcwd(), img_name)):
-        os.rename(os.path.join(getcwd(), img_name), os.path.join(getcwd() + "/Images", img_name))
+  	f = open("enrolled.txt", "a")
+  	f.write(name + "^^" + img_name)
+  	f.close()
+	for img_name in os.listdir(getcwd()):
+    	if os.path.isfile(os.path.join(getcwd(), img_name)):
+    		os.rename(os.path.join(getcwd(), img_name), os.path.join(getcwd() + "/Images", img_name))
 
   
 if __name__== "__main__":
