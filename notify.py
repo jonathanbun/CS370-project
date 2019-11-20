d03a2500d4fe0039dfa33a1d21fb8d76927d91d8
mercuryconfetti <jgarcia6102@gmail.com>
2019-11-20 15:28:40 -0700

    oof

---
+++ notify.py
@@ -0,0 +1,60 @@
+"""
+notification module
+"""
+import smtplib
+from email.mime.application import MIMEApplication
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+import os
+
+# for the sake of oop
+class notifier:
+
+	def send(self, name ,recipientAddress,tempImage):
+
+		# email we're sending notifications from
+		fromEmail = "jgarcia6102@gmail.com"
+		# for the sake of security we will add the app password when we're testing it/demoing
+		# dont want my email password floating around on github
+		appPassword = ""
+		# to email is recipient which is an email address
+		toEmail = recipient
+
+		# create smtp object, login to the gmail smtp server
+		smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+
+		# enable encryption through tls
+		smtpObj.starttls()
+
+		# initial server handshake
+		smtpObj.ehlo()
+
+		# login to the email address
+		smtpObj.login(fromEmail, appPassword)
+
+		# create a msg
+		msg = MIMEMultipart()
+
+		msg['From'] = fromEmail
+		# if name is unknown say so, else say who is at the door
+		if name == "Unknown":
+
+			msg['Subject'] = "Unknown is at the door!"
+
+		else:
+
+			msg['Subject'] = name + "is at the door!"
+		# read the image
+		readImage = open(tempImage, 'rb').read()
+
+		image = MIMEImage(readImage, name=tempImage)
+		# attach image to the message
+		msg.attach(image)
+
+		smtpObj.sendmail(readImage, toEmail, msg.as_String())
+
+		smtpObj.quit()
+
+
+
+
