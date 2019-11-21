b0dbe5e75f8eb4b8b10ecb8f33efb2122018c32d
suyashhiray <46005766+suyashhiray@users.noreply.github.com>
2019-11-21 15:03:04 -0700

    Update facerec.py

--- facerec.py
+++ facerec.py
@@ -40,20 +40,20 @@ with open('enrolled.txt') as fp:
 face_locations = []
 face_encodings = []
 face_names = []
-process_this_frame = True
+process_this_frame = 0
 
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
     # Resize frame of video to 1/4 size for faster face recognition processing
-    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    small_frame = cv2.resize(frame, (0, 0), fx=0.1, fy=0.1)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
 
     # Only process every other frame of video to save time
-    if process_this_frame:
+    if process_this_frame == 4:
         # Find all the faces and face encodings in the current frame of video
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
@@ -76,17 +76,18 @@ while True:
                 name = known_face_names[best_match_index]
 
             face_names.append(name)
+        process_this_frame = 0
 
-    process_this_frame = not process_this_frame
+    process_this_frame = process_this_frame + 1
 
 
     # Display the results
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
-        top *= 4
-        right *= 4
-        bottom *= 4
-        left *= 4
+        top *= 10
+        right *= 10
+        bottom *= 10
+        left *= 10
 
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
