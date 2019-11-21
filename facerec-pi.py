06c88d09643412c08357266a680d6f1bba95a934
suyashhiray <46005766+suyashhiray@users.noreply.github.com>
2019-11-21 14:02:47 -0700

    Update facerec-pi.py

--- facerec-pi.py
+++ facerec-pi.py
@@ -69,10 +69,11 @@ while True:
 
     # Resize frame of video to 1/4 size for faster face recognition processing
     # frame = cv2.imread(frame)
+    frame = output
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-    rgb_small_frame = small_frame[:, :, ::-1]
+    # rgb_small_frame = small_frame[:, :, ::-1]
 
     # Only process every other frame of video to save time
     if process_this_frame:
