4d94ad90be1a0b01aea88ab83a68382ee959b981
Jonathan Bunnell <jonathanbunnell@Jonathans-MBP.hsd1.co.comcast.net>
2019-12-01 18:41:28 -0700

    t

--- test2.py
+++ test2.py
@@ -88,9 +88,9 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
             self.end_headers()
             try:
                 while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
+                    with StreamingHandler.output.condition:
+                        StreamingHandler.output.condition.wait()
+                        frame = StreamingHandler.output.frame
                     self.wfile.write(b'--FRAME\r\n')
                     self.send_header('Content-Type', 'image/jpeg')
                     self.send_header('Content-Length', len(frame))
@@ -161,79 +161,64 @@ def run():
 
     video_capture = cv2.VideoCapture(0)
 
-    b = True
-
     while True:
-        if not b:
-            # Grab a single frame of video
-            ret, frame = video_capture.read()
+        # Grab a single frame of video
+        ret, frame = video_capture.read()
 
-            # Resize frame of video to 1/4 size for faster face recognition processing
-            small_frame = cv2.resize(frame, (0, 0), fx=0.2, fy=0.2)
+        # Resize frame of video to 1/4 size for faster face recognition processing
+        small_frame = cv2.resize(frame, (0, 0), fx=0.2, fy=0.2)
 
-            # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
-            rgb_small_frame = small_frame[:, :, ::-1]
+        # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
+        rgb_small_frame = small_frame[:, :, ::-1]
 
-            # Only process every other frame of video to save time
-            if process_this_frame == 4:
-                # Find all the faces and face encodings in the current frame of video
-                face_locations = face_recognition.face_locations(rgb_small_frame)
-                face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+        # Only process every other frame of video to save time
+        if process_this_frame == 4:
+            # Find all the faces and face encodings in the current frame of video
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-                face_names = []
-                for face_encoding in face_encodings:
-                    # See if the face is a match for the known face(s)
-                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                    name = "Unknown"
+            face_names = []
+            for face_encoding in face_encodings:
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+                name = "Unknown"
 
-                    # # If a match was found in known_face_encodings, just use the first one.
-                    if True in matches:
-                        first_match_index = matches.index(True)
-                        name = known_face_names[first_match_index]
-                    if False in matches:
-                        print("Unknown")
-                        b = False
+                # # If a match was found in known_face_encodings, just use the first one.
+                if True in matches:
+                    first_match_index = matches.index(True)
+                    name = known_face_names[first_match_index]
+                if False in matches:
+                    print("Unknown")
+                    video_capture.release()
+                    cv2.destroyAllWindows()
+                    time.sleep(2)
+                    run2()
 
-                        
+                
 
-                    
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = known_face_names[best_match_index]
 
-                    # Or instead, use the known face with the smallest distance to the new face
-                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                    best_match_index = np.argmin(face_distances)
-                    if matches[best_match_index]:
-                        name = known_face_names[best_match_index]
+                face_names.append(name)
+            process_this_frame = 0
 
-                    face_names.append(name)
-                process_this_frame = 0
-
-            process_this_frame = process_this_frame + 1
+        process_this_frame = process_this_frame + 1
 
 
-            # Hit 'q' on the keyboard to quit!
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        # Hit 'q' on the keyboard to quit!
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
-        if b:
-            with picamera.PiCamera(resolution='640x480', framerate=24) as camera:
-                output = StreamingOutput()
-                print("past output")
-                camera.start_recording(output, format='mjpeg')
-                try:
-                    address = ('', 8000)
-                    server = StreamingServer(address, StreamingHandler, output)
-                    server.serve_forever()
-                finally:
-                    camera.stop_recording()
-
-        # Release handle to the webcam
-        video_capture.release()
-        cv2.destroyAllWindows()
+    # Release handle to the webcam
+    video_capture.release()
+    cv2.destroyAllWindows()
 
 
 
 
 run()
-
 
 
