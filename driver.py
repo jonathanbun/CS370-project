426c572b9114137d58d7b5a5cb2b088085f618f1
Jonathan Bunnell <jonathanbunnell@Jonathans-MacBook-Pro.local>
2019-12-04 13:14:19 -0700

    Add option to enroll face

--- driver.py
+++ driver.py
@@ -1,10 +1,12 @@
 import server
 import facerec
+import enroll
 import sys
 print("*************************************")
 print("*************************************")
 print("To run continuous video stream: press 1")
 print("To run facial recognition: press 2")
+print("To add face to list of known faces: press 3")
 print("To exit: press q")
 print("-------------------------------------")
 
@@ -16,6 +18,9 @@ while True:
 	if response == "2":
 		print("Facial recognition booting...")
 		facerec.run()
+	if response == "3":
+		print("Enrollment booting...")
+		enroll.main()
 	if response == "q":
 		print("Shutting down...")
 		sys.exit()
