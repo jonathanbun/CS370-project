37ae7a2bf9ae02493efc5e93e96f4e007842b455
Jonathan Bunnell <jonathanbunnell@Jonathans-MacBook-Pro.local>
2019-12-04 11:48:56 -0700

    t

--- driver.py
+++ driver.py
@@ -1,9 +1,21 @@
 import server
+import facerec
 print("#####################################")
-print("Type live for Live Stream or fr for facial recognition")
+print("#####################################")
+print("#####################################")
+print("To run continuous video stream: press 1")
+print("To run facial recognition: press 2")
+print("To exit: press q")
+print("-------------------------------------")
+
 while True:
 	response = input()
-	if response == "live":
+	if response == "2":
+		print("Continuous stream booting...")
 		server.run()
-	if response == "fr":
+	if response == "2":
+		print("Facial recognition booting..")
 		facerec.run()
+	if response == "3":
+		print("Shutting down...")
+		sys.exit()
