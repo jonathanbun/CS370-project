8467e09b229c1dece82fbe1cb9718bf85d33f84f
suyashhiray <46005766+suyashhiray@users.noreply.github.com>
2019-12-05 14:21:11 -0700

    Update enroll.py

--- enroll.py
+++ enroll.py
@@ -3,51 +3,17 @@ import cv2
 import numpy as np
 import os
 import shutil
-# from tkinter import filedialog
-# from tkinter import *
 
 
 def main():
 	img_name = ""
 	name = input("Welcome to enrollment! Please enter your name:\n")
-	decision = input("Enter '1' for uploading an image or '2' for taking an image with the webcam\n")
-	if decision == "1":
-		img_name = input("Please enter image name with extension and place into folder:\n")
+	img_name = input("Please enter image name with extension and place into folder:\n")
 
-		# root = Tk()
-		# root.filename = filedialog.askopenfilename(initialdir = "/", title = "Select Enrollment Image", filetypes = (("jpeg files", "*.jpg"), ("all files", "*.*")))
-		# img_name = root.filename
-	elif decision == "2":
-		print("Camera is opening, press spacebar to take image\n")
-		cam = cv2.VideoCapture(0)
-		cv2.namedWindow("Enrollment")
-		while True:
-			ret, frame = cam.read()
-			cv2.imshow("Enrollment", frame)
-			if not ret:
-				break
-			k = cv2.waitKey(1)
-
-			if k%256 == 27:
-		        # ESC pressed
-				print("Escape hit, closing...")
-				break
-			elif k%256 == 32:
-		        # SPACE pressed
-				img_name = "{}.jpg".format(name)
-				cv2.imwrite(img_name, frame)
-				print("{} added to enrolled list!".format(img_name))
-
-			cam.release()
-
-			cv2.destroyAllWindows()
 
 	f = open("enrolled.txt", "a")
 	f.write("\n"+name + "^^" + img_name)
 	f.close()
-	# for file in os.listdir(os.getcwd()):
-	# 	if file.endswith(".jpg"):
-	# 		shutil.move(os.getcwd(), os.path.join(os.getcwd() + "/Images", file))
 
 if __name__== "__main__":
 	main()
