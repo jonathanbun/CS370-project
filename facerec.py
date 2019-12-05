143912e604956358e7927333a27fea9d9f6d0309
Jonathan Bunnell <jonathanbunnell@Jonathans-MacBook-Pro.local>
2019-12-05 13:26:52 -0700

    t

--- facerec.py
+++ facerec.py
@@ -34,10 +34,14 @@ def run():
 
     print("Camera Now Running...")
     face_encodings = None
+    fps = 0
+    start = time.time()
+    finish = start + 60
 
-    while True:
+    while start < finish:
         # Grab a single frame of video
         ret, frame = video_capture.read()
+        fps +=1
 
         # Resize frame of video to 1/4 size for faster face recognition processing
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
@@ -115,6 +119,8 @@ def run():
             cv2.destroyAllWindows()
 
             break
+        start = time.time()
+    print(fps)
 
     # Release handle to the webcam
     video_capture.release()
