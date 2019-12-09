9ac052aee80c83e2764ddab005917a540eb21fd6
Jonathan Bunnell <jonathanbunnell@Jonathans-MacBook-Pro.local>
2019-12-09 12:58:08 -0700

    t

--- facerec.py
+++ facerec.py
@@ -42,6 +42,7 @@ def run():
     while True:
         # Hit 'q' on the keyboard to quit!
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            print("qqqqq")
             video_capture.release()
             cv2.destroyAllWindows()
 
